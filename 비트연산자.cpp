0443f9a5574a4551c13934687655f621c3dceea2
YeJin-Kim-code <109929675+YeJin-Kim-code@users.noreply.github.com>
2023-09-19 23:39:53 +0900

    Add files via upload

---
+++ 비트연산자.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+	int num1 = 15; // 00000000 00000000 00000000 00001111
+	int num2 = 20; // 00000000 00000000 00000000 00010100
+
+	int num3 = num1 & num2;
+
+	printf("AND 연산의 결과 : %d \n", num3);
+
+	num3 = num1 | num2;
+
+	printf("OR 연산의 결과 : %d \n", num3);
+
+	num3 = num1 ^ num2;
+
+	printf("XOR 연산의 결과 : %d \n", num3);
+
+	num3 = ~num1;
+
+	printf("XOR 연산의 결과 : %d \n", num3);
+
+	return 0;
+}
