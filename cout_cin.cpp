6cfc66349a4309734313ef2397d5568c7bfbb728
YeJin-Kim-code <109929675+YeJin-Kim-code@users.noreply.github.com>
2023-09-20 23:37:26 +0900

    Add files via upload

---
+++ cout_cin.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+	int favorite_number;
+
+	std::cout << "1과 100사이에서 가장 좋아하는 숫자를 입력 해보세요: ";
+	std::cin >> favorite_number;
+	std::cout << "Amazing!! That's my favorite number too!" << std::endl;
+
+	return 0;//optional
+}
